541f42d94f8d0a2c8c6f667723b5ed7662efe324
bojana.koteska <bojana.koteska@localhost>
2024-06-01 09:56:02 +0200

    new

--- SchrodingerAPI/src/main/resources/dvr_py-master/3d_Hermite_Sho.py
+++
@@ -1,6 +0,0 @@
-import dvr_3d_copy
-import dvr_1d_copy
-import sys
-d1d = dvr_1d_copy.HermiteDVR(npts=int(sys.argv[1]))
-d3d = dvr_3d_copy.DVR(dvr1d=d1d)
-d3d.sho_test(k=float(sys.argv[2]),num_eigs=int(sys.argv[1]),x0=float(sys.argv[3]),y0=float(sys.argv[4]),z0=float(sys.argv[5]),precision=int(sys.argv[6]))
--- SchrodingerAPI/target/classes/dvr_py-master/2d_Hermite_Sho.py
+++
@@ -1,6 +0,0 @@
-import dvr_2d_copy
-import dvr_1d_copy
-import sys
-d1d = dvr_1d_copy.HermiteDVR(npts=int(sys.argv[1]))
-d2d = dvr_2d_copy.DVR(dvr1d=d1d)
-d2d.sho_test(k=float(sys.argv[2]),num_eigs=int(sys.argv[1]),x0=float(sys.argv[3]),y0=float(sys.argv[4]),precision=int(sys.argv[5]))
